38b309e07809d7da2abb20871b418505a29c5658
Nico Pampe <npampe@mines.edu>
2016-08-08 15:01:19 -0600

    Have a user input working. Was stuck on trailing newline char. Trim was solution

--- dnd_playersheets/src/main.rs
+++ dnd_playersheets/src/main.rs
@@ -4,10 +4,42 @@ pub mod character;
 use hero::Hero;
 use character::Character;
 
+use std::io;
+use std::string::String;
+use std::convert::AsRef;
+
+
 fn main() {
     println!("Hello, world!");
 
     let mut kurt: Hero = Character::new("Kurt");
 
     kurt.talk();
+
+    loop {
+        let mut user_input = String::new();
+
+        io::stdin().read_line(&mut user_input)
+            .expect("Failed to read line");
+
+        let user_input = user_input.trim();
+
+        match user_input.as_ref() {
+            "list" => println!("It's a list"),
+            "exit" => {
+                println!("Done!");
+                break;
+            },
+            _ => println!("nothing was entered {}", user_input),
+        }
+
+            // let mut input = String::new();
+
+        // io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        // match input.as_str() {
+        //     "test" => println!("That was test"),
+        //     _ => println!("Got some other input")
+        // }
+    }
 }
