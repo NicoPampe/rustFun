1a7bdafbbe2e07aca3e152555e966a703624928a
Nico Pampe <npampe@mines.edu>
2016-08-08 11:00:05 -0600

    Playing with structs, traits, ect

--- mud_game/src/main.rs
+++ mud_game/src/main.rs
@@ -1,3 +1,22 @@
+// extern crate rand;
+
+// use std::io;	
+// use std::cmp::Ordering;
+// use rand::Rng;
+
+struct Hero {
+	name: String,
+	age: i32,
+}
+
+trait Charater {
+	// fn new(name: &'static)
+	// http://rustbyexample.com/trait.html
+}
+
 fn main() {
     println!("Hello, world!");
+
+    let mut player = Hero { name: "Nico".to_string(), age: 21 };
+    println!("({}, {})", player.name, player.age);
 }
